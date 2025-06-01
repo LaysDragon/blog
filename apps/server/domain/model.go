c89a1f060c80caa99dd1b585a212c24a6825bf9d
LaysDragon <laysdra7265@gmail.com>
2025-06-01 15:05:29 +0800

    feat: built basic domain object

--- apps/server/domain/model.go
+++ apps/server/domain/model.go
@@ -1,6 +1,53 @@
 package domain
 
+import "time"
+
+type Account struct {
+	Id          int
+	CreatedDate time.Time
+	UpdatedDate time.Time
+	Username    string
+	Role        string
+	Email       string
+	PasswdHash  string
+}
+
+type Site struct {
+	Id          int
+	CreatedDate time.Time
+	UpdatedDate time.Time
+	Name        string
+}
+
 type Post struct {
-	Id      int
-	Content string
+	Id          int
+	CreatedDate time.Time
+	UpdatedDate time.Time
+	SiteId      int
+	Content     string
+}
+
+type Attachtment struct {
+	Id          int
+	CreatedDate time.Time
+	UpdatedDate time.Time
+	SiteId      int
+	RelatedId   int
+	Url         string
+}
+
+type Comment struct {
+	Id          int
+	CreatedDate time.Time
+	PostId      time.Time
+	Email       string
+	Name        string
+	Content     string
+}
+
+type AccessLog struct {
+	Id        int
+	Timestamp time.Time
+	UserId    int
+	Method    string
 }
