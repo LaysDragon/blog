d5ff7b8984e947e0ce6f1fb9c7c0655e6a6b76ac
LaysDragon <laysdra7265@gmail.com>
2025-06-02 00:40:04 +0800

    feat: extract config logic

---
+++ apps/server/internal/config.go
@@ -0,0 +1,31 @@
+package internal
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+// TODO: add config check
+type Config struct {
+	DBType         string
+	DataSourceName string
+}
+
+func LoadConfig() Config {
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileAlreadyExistsError); ok {
+			log.Fatalf("找不到配置檔:%v", err)
+		}
+		log.Fatalf("fatal error config file: %v", err)
+	}
+
+	var config Config
+	if err := viper.Unmarshal(&config); err != nil {
+		log.Fatalf("unable to decode into struct, %v ", err)
+	}
+	log.Printf("config: %vF", config)
+	return config
+}
--- apps/server/main.go
+++ apps/server/main.go
@@ -2,41 +2,19 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 
 	_ "github.com/lib/pq"
-	"github.com/spf13/viper"
 
 	"github.com/LaysDragon/blog/apps/server/db/pgrepo"
+	"github.com/LaysDragon/blog/apps/server/internal"
 	"github.com/LaysDragon/blog/apps/server/usecase"
 	"github.com/LaysDragon/blog/apps/server/web"
 	"github.com/gin-gonic/gin"
 )
 
-// TODO: add config check
-type Config struct {
-	DBType         string
-	DataSourceName string
-}
-
 func main() {
-	fmt.Println("hello world")
-	viper.SetConfigName("config")
-	viper.AddConfigPath(".")
-	if err := viper.ReadInConfig(); err != nil {
-		if _, ok := err.(viper.ConfigFileAlreadyExistsError); ok {
-			log.Fatalf("找不到配置檔:%v", err)
-		}
-		log.Fatalf("fatal error config file: %v", err)
-	}
-
-	var config Config
-	fmt.Println(viper.AllSettings())
-	if err := viper.Unmarshal(&config); err != nil {
-		log.Fatalf("unable to decode into struct, %v ", err)
-	}
-	log.Printf("config: %v", config)
+	config := internal.LoadConfig()
 
 	db, err := sql.Open(config.DBType, config.DataSourceName)
 	if err != nil {
