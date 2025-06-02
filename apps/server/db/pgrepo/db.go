fd7c5665fc0491afad4d4013388755b57bbe8bf5
LaysDragon <laysdra7265@gmail.com>
2025-06-02 21:54:28 +0800

    feat:refactor postdb

--- apps/server/db/pgrepo/db.go
+++ apps/server/db/pgrepo/db.go
@@ -5,11 +5,8 @@ import (
 	"database/sql"
 	"errors"
 
-	"github.com/LaysDragon/blog/apps/server/db/pgrepo/models"
-	"github.com/LaysDragon/blog/apps/server/domain"
 	usecase "github.com/LaysDragon/blog/apps/server/usecase"
 	"github.com/volatiletech/sqlboiler/v4/boil"
-	// . "github.com/volatiletech/sqlboiler/v4/queries/qm"
 )
 
 type CommonDb[T usecase.CommonRepo[T]] struct {
@@ -17,7 +14,7 @@ type CommonDb[T usecase.CommonRepo[T]] struct {
 	builder func(db boil.ContextExecutor) T
 }
 
-func nilType[T any]() T {
+func nilVal[T any]() T {
 	var t T
 	return t
 }
@@ -26,11 +23,11 @@ func (c *CommonDb[T]) BeginTx(ctx context.Context) (T, error) {
 	if db, ok := c.db.(*sql.DB); ok {
 		tx, err := db.BeginTx(ctx, nil)
 		if err != nil {
-			return nilType[T](), err
+			return nilVal[T](), err
 		}
 		return c.builder(tx), nil
 	}
-	return nilType[T](), errors.New("not *sql.DB")
+	return nilVal[T](), errors.New("not *sql.DB")
 }
 
 func (c *CommonDb[T]) Commit() error {
@@ -47,65 +44,9 @@ func (c *CommonDb[T]) Rollback() error {
 	return errors.New("not *sql.Tx")
 }
 
-type PostDb struct {
-	CommonDb[usecase.PostRepo]
-}
-
-func NewPost(db boil.ContextExecutor) usecase.PostRepo {
-	a := NewPost
-	return &PostDb{
-		CommonDb: CommonDb[usecase.PostRepo]{
-			db:      db,
-			builder: a,
-		},
+func ErrorTranslate(err error) error {
+	if err == sql.ErrNoRows {
+		return &usecase.ItemNotExistedError{Err: err}
 	}
-}
-
-func (r *PostDb) ToDb(post *domain.Post) *models.Post {
-	if post == nil {
-		return nil
-	}
-	return &models.Post{
-		ID:        post.Id,
-		CreatedAt: post.CreatedAt,
-		UpdatedAt: post.UpdatedAt,
-		SiteID:    post.SiteId,
-		Content:   post.Content,
-	}
-}
-
-func (r *PostDb) ToDomain(post *models.Post) *domain.Post {
-	if post == nil {
-		return nil
-	}
-	return &domain.Post{
-		Id:        post.ID,
-		CreatedAt: post.CreatedAt,
-		UpdatedAt: post.UpdatedAt,
-		SiteId:    post.SiteID,
-		Content:   post.Content,
-	}
-}
-
-func (r *PostDb) ById(ctx context.Context, id int) (*domain.Post, error) {
-	post, err := models.FindPost(ctx, r.db, id)
-
-	if err != nil {
-		return nil, err
-	}
-	return r.ToDomain(post), nil
-}
-
-func (r *PostDb) Upsert(ctx context.Context, post *domain.Post) (*domain.Post, error) {
-	dbPost := r.ToDb(post)
-	err := dbPost.Upsert(ctx, r.db, true, []string{"id"}, boil.Infer(), boil.Infer())
-	if err != nil {
-		return nil, err
-	}
-	return r.ToDomain(dbPost), nil
-}
-
-func (r *PostDb) Delete(ctx context.Context, id int) error {
-	_, err := models.Posts(models.PostWhere.ID.EQ(id)).DeleteAll(ctx, r.db, false)
 	return err
 }
---
+++ apps/server/db/pgrepo/post.go
@@ -0,0 +1,73 @@
+package pgrepo
+
+import (
+	"context"
+
+	"github.com/LaysDragon/blog/apps/server/db/pgrepo/models"
+	"github.com/LaysDragon/blog/apps/server/domain"
+	usecase "github.com/LaysDragon/blog/apps/server/usecase"
+	"github.com/volatiletech/sqlboiler/v4/boil"
+	// . "github.com/volatiletech/sqlboiler/v4/queries/qm"
+)
+
+type PostDb struct {
+	CommonDb[usecase.PostRepo]
+}
+
+func NewPost(db boil.ContextExecutor) usecase.PostRepo {
+	return &PostDb{
+		CommonDb: CommonDb[usecase.PostRepo]{
+			db:      db,
+			builder: NewPost,
+		},
+	}
+}
+
+func (r *PostDb) ToDb(post *domain.Post) *models.Post {
+	if post == nil {
+		return nil
+	}
+	return &models.Post{
+		ID:        post.Id,
+		CreatedAt: post.CreatedAt,
+		UpdatedAt: post.UpdatedAt,
+		SiteID:    post.SiteId,
+		Content:   post.Content,
+	}
+}
+
+func (r *PostDb) ToDomain(post *models.Post) *domain.Post {
+	if post == nil {
+		return nil
+	}
+	return &domain.Post{
+		Id:        post.ID,
+		CreatedAt: post.CreatedAt,
+		UpdatedAt: post.UpdatedAt,
+		SiteId:    post.SiteID,
+		Content:   post.Content,
+	}
+}
+
+func (r *PostDb) ById(ctx context.Context, id int) (*domain.Post, error) {
+	post, err := models.FindPost(ctx, r.db, id)
+
+	if err != nil {
+		return nil, ErrorTranslate(err)
+	}
+	return r.ToDomain(post), nil
+}
+
+func (r *PostDb) Upsert(ctx context.Context, post *domain.Post) (*domain.Post, error) {
+	dbPost := r.ToDb(post)
+	err := dbPost.Upsert(ctx, r.db, true, []string{"id"}, boil.Infer(), boil.Infer())
+	if err != nil {
+		return nil, err
+	}
+	return r.ToDomain(dbPost), nil
+}
+
+func (r *PostDb) Delete(ctx context.Context, id int) error {
+	_, err := models.Posts(models.PostWhere.ID.EQ(id)).DeleteAll(ctx, r.db, false)
+	return ErrorTranslate(err)
+}
