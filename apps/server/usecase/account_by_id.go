0e2721a3c4279e7856c213aba221ea44d323fe52
LaysDragon <laysdra7265@gmail.com>
2025-06-14 19:08:31 +0800

    feat: account get implement perm check

--- apps/server/usecase/account_by_id.go
+++ apps/server/usecase/account_by_id.go
@@ -4,8 +4,13 @@ import (
 	"context"
 
 	"github.com/LaysDragon/blog/apps/server/domain"
+	"github.com/LaysDragon/blog/apps/server/perm"
 )
 
-func (s *Account) ById(ctx context.Context, id int) (*domain.Account, error) {
-	return s.repo.ById(ctx, id)
+func (a *Account) ById(ctx context.Context, op perm.ResId, id int) (*domain.Account, error) {
+	err := a.perm.CheckE(op, perm.ACT_READ, perm.User(id))
+	if err != nil {
+		return nil, err
+	}
+	return a.repo.ById(ctx, id)
 }
--- apps/server/web/account_get.go
+++ apps/server/web/account_get.go
@@ -1,9 +1,11 @@
 package web
 
 import (
+	"errors"
 	"net/http"
 	"strconv"
 
+	"github.com/LaysDragon/blog/apps/server/perm"
 	"github.com/LaysDragon/blog/apps/server/usecase"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
@@ -15,16 +17,17 @@ func (c *AccountController) HandleGet(ctx *gin.Context) {
 		ctx.Status(http.StatusBadRequest)
 	}
 
-	//TODO: add permission check
-
-	acc, err := c.usecase.ById(ctx, id)
+	acc, err := c.usecase.ById(ctx, GetUserOp(ctx), id)
 	if err != nil {
-		if _, ok := err.(usecase.ItemNotExistedError); ok {
-			ctx.Status(http.StatusNotFound)
-			return
-		}
 		c.log.Error("Failed to retrived account data", zap.Int("id", id), zap.Error(err))
-		ctx.Status(http.StatusInternalServerError)
+		switch {
+		case errors.Is(err, usecase.ItemNotExistedError{}):
+			ctx.Status(http.StatusNotFound)
+		case errors.Is(err, perm.PermissionError{}):
+			ctx.Status(http.StatusForbidden)
+		default:
+			ctx.Status(http.StatusInternalServerError)
+		}
 		return
 	}
 	ctx.JSON(http.StatusOK, c.ToDto(acc))
